93567eaad591c7cae631ca07dc7a88067d6dcaa3
caoshiping <soraping@163.com>
2022-03-17 22:06:53 +0800

    '工厂函数'

--- src/generic-function/factory.ts
+++ src/generic-function/factory.ts
@@ -1,8 +1,10 @@
-class Person {
-    constructor(private name: string){
+class Person1 {
+    constructor(private name1: string){
+
     }
     getName(){
-        console.log(this.name)
+        console.log(this.name1)
+        
     }
 }
 
@@ -10,9 +12,12 @@ type TConstructor<T> = new (...args: any) => T
 
 
 function createConstructFactory<T>(Con: TConstructor<T>): T{
-    console.log("construct")
+    // 构造函数也是 Fuction ，Fuction type 没有 name 属性，只能通过tostring 方法截取
+    // console.log("constructor", Con.toString())
+    let reg = /function (.{1,})\(/;
+    console.log("constructor name", reg.exec(Con.toString())![1])
     return new Con("zhangsan")
 }
 
-let p = createConstructFactory(Person)
+let p = createConstructFactory<Person1>(Person1)
 p.getName()
